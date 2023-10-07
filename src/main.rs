6178b7e3d3c91f5931f9669628cb231d79fa1066
Henrick Deschamps <henrick.deschamps@gmail.com>
2023-10-07 09:38:49 +0200

    [Opt] Add opt parsing and rom path arg
    Using Clap v4.4 with derive feature

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = env!("CARGO_PKG_DESCRIPTION"))]
+struct Opt {
+    #[clap(short = 'r', long = "rom", required = true)]
+    rom: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::parse();
+    let rom_path: &String = &opt.rom;
 }
