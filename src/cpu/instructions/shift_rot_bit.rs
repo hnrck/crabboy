de3a1dfcf340cadc1ca803a4a427558636d0bcf7
Henrick Deschamps <henrick.deschamps@gmail.com>
2023-12-27 19:10:20 +0100

    [CPU] Implement 8-bit srb instructions

--- src/cpu/instructions/shift_rot_bit.rs
+++ src/cpu/instructions/shift_rot_bit.rs
@@ -1,11 +1,67 @@
 use std::collections::HashMap;
 
-use crate::cpu::instructions::Instruction;
+use crate::cpu::instructions::{Cycles, Instruction};
 
 pub(super) fn instructions_map_shift_rot_bit_instructions(instructions_map: &mut HashMap<u8, Instruction>) -> () {
     instructions_map_8_bit_shift_rot_bit_instructions(instructions_map)
 }
 
-fn instructions_map_8_bit_shift_rot_bit_instructions(_instructions_map: &mut HashMap<u8, Instruction>) -> () {
-    // TODO(henrick) Implementation
+fn instructions_map_8_bit_shift_rot_bit_instructions(instructions_map: &mut HashMap<u8, Instruction>) -> () {
+    instructions_map.insert(
+        0x07, Instruction::new(
+            "RLCA", |registers, memory| {
+                let carry = (registers.a & 0x80) != 0;
+                registers.a = (registers.a << 1) | (carry as u8);
+                registers.f.z = false;
+                registers.f.n = false;
+                registers.f.h = false;
+                registers.f.c = carry;
+                (true, true)
+            }, Cycles::new(4), 1,
+        ),
+    );
+
+    instructions_map.insert(
+        0x17, Instruction::new(
+            "RLA", |registers, memory| {
+                let carry = (registers.a & 0x80) != 0;
+                registers.a = (registers.a << 1) | (registers.f.c as u8);
+                registers.f.z = false;
+                registers.f.n = false;
+                registers.f.h = false;
+                registers.f.c = carry;
+                (true, true)
+            }, Cycles::new(4), 1,
+        ),
+    );
+
+    instructions_map.insert(
+        0x0F, Instruction::new(
+            "RRCA", |registers, memory| {
+                let carry = registers.a & 0x01 != 0;
+                registers.a = ((carry as u8) << 7) | (registers.a >> 1);
+                registers.f.z = false;
+                registers.f.n = false;
+                registers.f.h = false;
+                registers.f.c = carry;
+                (true, true)
+            }, Cycles::new(4), 1,
+        ),
+    );
+
+    instructions_map.insert(
+        0x1F, Instruction::new(
+            "RRA", |registers, memory| {
+                let carry = registers.a & 0x01 != 0;
+                registers.a = ((registers.f.c as u8) << 7) | (registers.a >> 1);
+                registers.f.z = false;
+                registers.f.n = false;
+                registers.f.h = false;
+                registers.f.c = carry;
+                (true, true)
+            }, Cycles::new(4), 1,
+        ),
+    );
 }
+
+// TODO(henrick) Prefix CB
