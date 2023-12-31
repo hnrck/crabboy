eca7c969f98d2cb35123d2a62c1e27b9802eec08
Henrick Deschamps <henrick.deschamps@gmail.com>
2023-12-31 14:56:34 +0100

    [Refacto] Improve main and add doc

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,28 @@ mod cartridge;
 mod rom;
 mod cpu;
 
+/// Retrieve the value of the `CARGO_PKG_VERSION` environment variable.
+const OPT_SETUP: &'static str = env!("CARGO_PKG_VERSION");
+
+/// Retrieve the value of the `CARGO_PKG_AUTHORS` environment variable.
+const AUTHOR_SETUP: &'static str = env!("CARGO_PKG_AUTHORS");
+
+/// Retrieve the value of the `CARGO_PKG_DESCRIPTION` environment variable.
+const ABOUT_SETUP: &'static str = env!("CARGO_PKG_DESCRIPTION");
+
+/// Command-line options
 #[derive(Parser)]
-#[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = env!("CARGO_PKG_DESCRIPTION"))]
+#[clap(version = OPT_SETUP, author = AUTHOR_SETUP, about = ABOUT_SETUP)]
 struct Opt {
+    /// The command line argument for specifying the path to a ROM file.
     #[clap(short = 'r', long = "rom", required = true)]
     rom: String,
 }
 
+/// Initializes the logger with debug level filtering if the `debug_assertions` feature is enabled.
+///
+/// This function should be called once at the start of the application to initialize the logger
+/// and set the desired log level.
 #[cfg(debug_assertions)]
 fn init_logger() {
     env_logger::builder()
@@ -27,6 +42,9 @@ fn init_logger() {
         .init();
 }
 
+/// Initializes the logger based on the current configuration.
+///
+/// The logger is initialized with a log level of `Info` when the program is not compiled with assertions enabled.
 #[cfg(not(debug_assertions))]
 fn init_logger() {
     env_logger::builder()
@@ -34,21 +52,45 @@ fn init_logger() {
         .init();
 }
 
+/// Runs the ROM on the emulator.
+///
+/// # Arguments
+///
+/// * `rom` - The ROM to be loaded and executed.
+///
+/// # Examples
+///
+/// ```rust
+/// let rom = Rom::new("game.rom");
+/// run_rom(rom);
+/// ```
+///
+/// # Panics
+///
+/// This function will panic if there is an error loading or validating the ROM.
+///
+/// # Safety
+///
+/// This function assumes that the ROM has been loaded and validated successfully.
+fn run_rom(rom: Rom) {
+    debug!("ROM loaded and validated successfully");
+    rom.print_info();
+    let mut mmu = MMU::new().with_rom(rom);
+    let mut cpu = CPU::new();
+    loop {
+        // TODO(henrick) Step error handling
+        cpu.step(&mut mmu);
+    }
+}
+
+/// Main
 fn main() {
     init_logger();
     let opt = Opt::parse();
-    let rom_path: &String = &opt.rom;
-    match Rom::from_path(&rom_path) {
+    match Rom::from_path(&opt.rom) {
         Ok(rom) => {
             if rom.validate() {
-                debug!("ROM loaded and validated successfully");
-                rom.print_info();
-                let mut mmu = MMU::new().with_rom(rom);
-                let mut cpu = CPU::new();
-                loop {
-                    // TODO(henrick) Step error handling
-                    cpu.step(&mut mmu);
-                }
+                run_rom(rom);
             } else {
                 error!("Invalid ROM!");
                 process::exit(1);
